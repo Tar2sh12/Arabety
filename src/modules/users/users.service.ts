f50575c19f73938745cc2f23f1157ed03f59710e
mohamed tarek <mohamad1tarek1@gmail.com>
2025-03-05 16:45:56 +0200

    s

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import {Repository }from 'typeorm';
 import {InjectRepository }from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -26,7 +26,7 @@ export class UsersService {
     async update(id:number , attrs:UpdateUserDto){
         const user =await this.repo.findOne({where:{id}});
         if(!user){
-            throw new Error('user not found');
+            throw new NotFoundException('user not found');
         }
 
 
@@ -44,7 +44,7 @@ export class UsersService {
     async remove(id:number){
         const user = await this.repo.findOne({where:{id}});
         if(!user){
-            throw new Error('user not found');
+            throw new NotFoundException('user not found');
         }
         const deletedUser = await this.repo.remove(user);
         return {
